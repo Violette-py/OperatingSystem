6f408a35d709c4776af0adfebe8e0a7cdfaa423c
PeiyiChen <peiyichen0614@gmail.com>
2023-10-03 10:54:51 +0800

    🎉 init: test

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -1,7 +1,7 @@
 // Physical memory allocator, for user processes,
 // kernel stacks, page-table pages,
 // and pipe buffers. Allocates whole 4096-byte pages.
-
+// here is a test
 #include "types.h"
 #include "param.h"
 #include "memlayout.h"
