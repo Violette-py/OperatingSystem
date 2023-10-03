96cf5271de50d2914987ff86836596431ef18de9
PeiyiChen <peiyichen0614@gmail.com>
2023-10-03 23:06:31 +0800

    ✨ feat: kalloc_but still with panic bugs

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -1,7 +1,7 @@
 // Physical memory allocator, for user processes,
 // kernel stacks, page-table pages,
 // and pipe buffers. Allocates whole 4096-byte pages.
-// here is a test
+
 #include "types.h"
 #include "param.h"
 #include "memlayout.h"
@@ -9,10 +9,13 @@
 #include "riscv.h"
 #include "defs.h"
 
+#include "proc.h"
+
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
+                   // FIXME: why char* ???
 
 struct run {
   struct run *next;
@@ -21,13 +24,17 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+  char lock_name[7];
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+  for(int i = 0; i < NCPU; i++) {
+    snprintf(kmem[i].lock_name, sizeof(kmem[i].lock_name), "kmem_%d", i);
+    initlock(&kmem[i].lock, kmem[i].lock_name);
+  }
+  freerange(end, (void*)PHYSTOP);      
 }
 
 void
@@ -56,10 +63,17 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();
+  int cpu_id = cpuid();
+  // pop_off();
+
+  acquire(&kmem[cpu_id].lock);
+  r->next = kmem[cpu_id].freelist;
+  kmem[cpu_id].freelist = r;
+  release(&kmem[cpu_id].lock);
+
+  pop_off();
+
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,11 +84,43 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  // NOTE: The function cpuid returns the current core number, but it's only safe to call it and use its result when interrupts are turned off. You should use push_off() and pop_off() to turn interrupts off and on.
+
+  push_off();
+  int cpu_id = cpuid();
+  // pop_off();
+
+  // FIXME: 当2个cpu试图互相借用时，可能会出现死锁 -- 当两个锁的调用和释放有交叉时，注死锁的问题
+  // FIXME: 释放自己的锁再去偷页，把偷的页头指针保存起来，再申请本cpu的锁来更新空闲链表？
+
+  acquire(&kmem[cpu_id].lock);
+  r = kmem[cpu_id].freelist;
+
+  if(r) {
+    kmem[cpu_id].freelist = r->next;
+    release(&kmem[cpu_id].lock);
+
+  } else {
+    // current cpu has no freelist, so lend from other cpu
+
+    release(&kmem[cpu_id].lock);
+
+    int free_cpu;
+    
+    // FIXME: 目前只是顺序访问，没有使用tricks
+    for(int i = 0; i < NCPU; i++) {
+      free_cpu = (cpu_id + i) % NCPU;
+      if(kmem[free_cpu].freelist) {
+        acquire(&kmem[free_cpu].lock);
+        r = kmem[free_cpu].freelist;
+        kmem[free_cpu].freelist = r->next;
+        release(&kmem[free_cpu].lock);
+        break;
+      }
+    }
+  }
+  
+  pop_off();
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
